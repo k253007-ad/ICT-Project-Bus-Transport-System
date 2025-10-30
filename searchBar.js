f053618f8fe09c70f98b8a64c6f572d2bc48bd42
Adnan_Ahmed_25K-3007 <k253007@nu.edu.pk>
2025-10-30 15:54:26 +0500

    Add files via upload

---
+++ searchBar.js
@@ -0,0 +1,22 @@
+const li = document.querySelectorAll("li");
+function filterList() {
+    const input = document.getElementById('searchInput');
+    const filter = input.value.toLowerCase();
+    const ul = document.getElementById('stopsList');
+
+    for (let i = 0; i < li.length; i++) {
+        const textValue = li[i].textContent || li[i].innerText;
+        if (textValue.toLowerCase().indexOf(filter) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+}
+
+
+li?.forEach(element => {
+    element.onclick = ()=>{ 
+        window.location.assign("/new.html")
+    }
+});
